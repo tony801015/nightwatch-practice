02a029d003cfebabacbdc4ff8fa12e416aaa02b0
tony801015 <jiawenluotony@email.com>
2016-11-18 23:49:28 +0800

    lol

--- demo-nw.js
+++ demo-nw.js
@@ -1,21 +1,44 @@
+var userName = 'tkringo';
 module.exports = {
     'test Yahoo': function (client) {
         client
-
-            .url('https://tw.yahoo.com')
-            .waitForElementVisible('body')
-            .setValue('input[type=text]', '美國總統大選')
-            .click('input[id=UHSearchWeb]')
-            .pause(2000)
-            .assert.containsText('div.sys_northalsotry', '美國總統大選2016日期')
-            .waitForElementVisible('div.sys_northalsotry')
+            .url('https://lol.moa.tw/summoner/show/' + userName)
+            .pause(6000)
+            .click('#tabs > ul > li:nth-child(4) > a')
+            .pause(5000)
             .source((result) => {
+                const userObject = [];
+
                 const cheerio = require("cheerio")
                 const $ = cheerio.load(result.value)
-                console.log($('div.sys_northalsotry').text());
+                var level1 = $('#match_overview > div.table-responsive > table > tbody > tr')
 
+
+                var person;
+                level1.each(function (i, elem) {
+                    const _$ = cheerio.load($(this).html());
+                    if (i === 20) {
+                        return;
+                    }
+                    if (i % 2 == 0) {
+                        person = {
+                            name: "",
+                            type: "",
+                            Avatars: ""
+                        };
+                        person.name = userName;
+
+                        person.type = _$('th > span:nth-child(3)').text();
+                    } else {
+                        person.Avatars = _$('td:nth-child(1) > div').attr('data-code');
+                        userObject.push(person);
+                    }
+
+
+                });
+                console.log(userObject);
             })
+            .pause(5000)
             .end();
-
     }
 };
